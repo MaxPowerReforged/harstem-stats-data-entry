ddf693cf914f04dafa5f83d34789fc2fc47e50a7
MaxPowerEdain <joancarazo1994@gmail.com>
2021-05-18 08:47:14 +0200

    fb0f4ba

---
+++ _nuxt/static/1621320392/grandmasterZerg/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fharstem-stats-data-entry\u002F_nuxt\u002Fstatic\u002F1621320392",layout:"default",error:a,serverRendered:true,routePath:"\u002FgrandmasterZerg",config:{_app:{basePath:"\u002Fharstem-stats-data-entry\u002F",assetsPath:"\u002Fharstem-stats-data-entry\u002F_nuxt\u002F",cdnURL:a}}}}(null));
